1c9caa8fbf445cb8ae795480bd756d9bb85a2cbc
Tanveer Noman <tanveer.noman@sebpo.com>
2014-04-29 13:52:39 +0600

    cleanup

--- app/models/address_model.php
+++ app/models/address_model.php
@@ -17,40 +17,40 @@ class Address_model extends Model {
 		return $result;
 	}
 
-        public function getAddress($id = null){
-            if($id != null){
-                $result = $this->query('SELECT * FROM `addresses` WHERE id ='.$this->escapeString($id));
-                return $result;
-            }
+    public function getAddress($id = null){
+        if($id != null){
+            $result = $this->query('SELECT * FROM `addresses` WHERE id ='.$this->escapeString($id));
+            return $result;
         }
+    }
 
-        public function add($array = array()){
-            if(!empty($array)){
-                $arr = $this->escapeArray($array);
-                $query = "INSERT INTO `addresses` (`first_name`, `last_name`, `street`, `zip`,`citiesid`)
-                VALUES('".$arr['first_name']."','".$arr['last_name']."','".$arr['street']."','".$arr['zip']."',".$arr['city'].")";
-                return $this->execute($query);
-            }
+    public function add($array = array()){
+        if(!empty($array)){
+            $arr = $this->escapeArray($array);
+            $query = "INSERT INTO `addresses` (`first_name`, `last_name`, `street`, `zip`,`citiesid`)
+            VALUES('".$arr['first_name']."','".$arr['last_name']."','".$arr['street']."','".$arr['zip']."',".$arr['city'].")";
+            return $this->execute($query);
         }
+    }
 
-        public function edit($array = array()){
-            if(!empty($array)){
-                $arr = $this->escapeArray($array); 
-                $query = "UPDATE `addresses` SET `first_name` = '".$arr['first_name']."',
-                    `last_name` = '".$arr['last_name']."',
-                    `street` = '".$arr['street']."',
-                    `zip` = '".$arr['zip']."',
-                    `citiesid` = ". $arr['city']." WHERE `id` = ".$arr['id'];
-                return $this->execute($query);
-            }
+    public function edit($array = array()){
+        if(!empty($array)){
+            $arr = $this->escapeArray($array); 
+            $query = "UPDATE `addresses` SET `first_name` = '".$arr['first_name']."',
+                `last_name` = '".$arr['last_name']."',
+                `street` = '".$arr['street']."',
+                `zip` = '".$arr['zip']."',
+                `citiesid` = ". $arr['city']." WHERE `id` = ".$arr['id'];
+            return $this->execute($query);
         }
+    }
 
-        public function delete($id = null){
-            if($id != null){
-                $query = 'DELETE FROM `addresses` WHERE `id` = '.$this->escapeString($id);
-                return $this->execute($query);
-            }
+    public function delete($id = null){
+        if($id != null){
+            $query = 'DELETE FROM `addresses` WHERE `id` = '.$this->escapeString($id);
+            return $this->execute($query);
         }
+    }
 }
 
 ?>
